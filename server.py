613ac187e1d6f3ff63ad88362d74e9ab1e84ae95
Abhi11341413 <121652839+Abhi11341413@users.noreply.github.com>
2026-05-01 19:55:03 +0530

    Agriguard Version-2

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import flwr as fl
+
 strategy = fl.server.strategy.FedAvg(
     min_fit_clients=3,
     min_available_clients=3
@@ -8,6 +9,6 @@ print("Starting AgriGuard Central Server... Waiting for farmers to connect.")
 
 fl.server.start_server(
     server_address="127.0.0.1:8080",
-    config=fl.server.ServerConfig(num_rounds=3),
+    config=fl.server.ServerConfig(num_rounds=20), # <--- Changed to 20 rounds!
     strategy=strategy
 )
